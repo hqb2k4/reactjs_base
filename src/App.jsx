e795b84dade94043b7c6384800c7c0231980d983
hqb4102k4 <hoangquocbao4102k4@gmail.com>
2025-08-05 17:00:18 +0700

    Add image display to Todo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Tododata from "./component/todo/todo_data/Tododata"
 import Todosearch from "./component/todo/todo_search/Todosearch"
-
 import './component/todo/style.css'
+import img from "./assets/react.svg"
 
 const App = () => {
   return ( 
@@ -9,6 +9,9 @@ const App = () => {
             <div className="Todo-title">My Todo List</div>
             <Todosearch />
             <Tododata />
+            <div className="Todo-image">
+                <img src={img}/>
+            </div>
         </div>
   )
 }
