8e38bb6c3be9a1035a0c05e8f3d5c3b0ef0982e8
hqb4102k4 <hoangquocbao4102k4@gmail.com>
2025-08-07 14:40:48 +0700

    Pass props to Tododata component for dynamic data rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import Todosearch from "./component/todo/todo_search/Todosearch"
 import './component/todo/style.css'
 import img from "./assets/react.svg"
 
+const FullName = "Hoang Quoc Bao";
+const Age = 21;
+const data = {
+  name: "Bao",
+  age: 21,
+  address: "Ha Noi"
+}
 const App = () => {
   return ( 
         <div className="Todo-container">
             <div className="Todo-title">My Todo List</div>
             <Todosearch />
-            <Tododata />
+            <Tododata 
+              name = {FullName}
+              age = {Age}
+              data = {data}
+            />
             <div className="Todo-image">
                 <img src={img}/>
             </div>
--- src/component/todo/todo_data/Tododata.jsx
+++ src/component/todo/todo_data/Tododata.jsx
@@ -1,6 +1,12 @@
-const Tododata = () => {
+const Tododata = (props) => {
+    // console.log(props)
+    // props là một object chứa các thuộc tính được truyền từ component cha
+
+    //destructuring props
+    const {name} = props;
     return (
         <div className="Todo-data">
+            <div className="Todo-item">Name: {name}</div>
             <div className="Todo-item">Reactjs</div>
             <div className="Todo-item">Vuejs</div>
             <div className="Todo-item">Angular</div>
