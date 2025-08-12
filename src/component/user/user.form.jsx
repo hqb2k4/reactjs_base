40e6258629b80190366c6cfa5acdafa315ad8542
hqb4102k4 <hoangquocbao4102k4@gmail.com>
2025-08-12 21:44:50 +0700

    use State Hook take value

--- src/component/user/user.form.jsx
+++ src/component/user/user.form.jsx
@@ -1,30 +1,58 @@
 import { Button, Input } from 'antd';
+import { useState } from 'react';
 const UserForm = () => {
+    //useState Hook UserForm
+    const [fullName, setFullName] = useState();
+    const [email, setEmail] = useState();
+    const [phone, setPhone] = useState();
+    const [password, setPassword] = useState();
+    const handleOnClick = () => {
+        console.log("User created:", { fullName, email, phone, password });
+    }
     return (
-        <div className='user-form' style={{margin: '20px 0'}}>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+        <div className='user-form' style={{ margin: '20px 0' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                 <div>
                     <span>FullName</span>
-                    <Input placeholder="FullName" />
-                </div>
-                 <div>
-                    <span>Email</span>
-                    <Input placeholder="Email" />
-                </div>
-                 <div>
-                    <span>Phone</span>
-                    <Input placeholder="Phone" />
-                </div>
-                 <div>
-                    <span>Password</span>
-                    <Input.Password placeholder="Password" />
+                    <Input
+                        placeholder="FullName"
+                        // props value UserForm to Input
+                        value={fullName}
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Creat User</Button>
+                    <span>Email</span>
+                    <Input
+                        placeholder="Email"
+                        value={email}
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        placeholder="Password"
+                        value={password}
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <span>Phone</span>
+                    <Input
+                        placeholder="Phone"
+                        value={phone}
+                        onChange={(event) => { setPhone(event.target.value) }}
+                    />
+                </div>
+                <div>
+                    <Button type="primary" onClick={handleOnClick}>
+                        Creat User
+                    </Button>
                 </div>
             </div>
-        </div>
-       
+        </div >
+
     );
 }
 export default UserForm;
